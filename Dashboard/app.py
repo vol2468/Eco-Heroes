ce2c663388a2072de371924a900c80b1a581ac17
joyfuluj <joyumejiego@yahoo.com>
2024-02-29 10:39:08 -0800

    created a project

---
+++ Dashboard/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+@app.route('/index')
+def index():
+    return render_template('index.html', name='PyCharm')
+
+if __name__ == '__main__':
+    app.run()
