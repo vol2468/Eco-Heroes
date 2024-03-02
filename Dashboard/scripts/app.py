00f94d3c058c56aca3c8cc36aa78035ef3d4b3c1
joyfuluj <joyumejiego@yahoo.com>
2024-03-02 13:53:49 -0800

    updated app.py

--- Dashboard/scripts/app.py
+++ Dashboard/scripts/app.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template, request
+import requests
+from index import index
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/')         
 def hello_world():  # put application's code here
     return 'Hello World!'
 
 @app.route('/index', methods=['GET', 'POST'])
-def index():
-    searchword = request.args.get('key', '')
-    return render_template('compare.html', name=searchword)
+def call_index():
+    result = index()
+    return result
+
+
+
 
 if __name__ == '__main__':
     app.run()
