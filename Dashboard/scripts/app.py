25e01c3d1b79a4516bebc0ff330f571d924adfbe
vol2468 <vol2468@student.ubc.ca>
2024-03-01 21:39:22 -0800

    login page

--- Dashboard/scripts/app.py
+++ Dashboard/scripts/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,9 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', name='Leo')
+    searchword = request.args.get('key', '')
+    return render_template('compare.html', name=searchword)
 
 if __name__ == '__main__':
     app.run()
