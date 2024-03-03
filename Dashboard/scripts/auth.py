38aae8c6c44c4c7119e3a65280c83b410bbbeec2
leolelion <144266057+leolelion@users.noreply.github.com>
2024-03-02 18:41:55 -0800

    auth compare add Dashboard to import

--- Dashboard/scripts/auth.py
+++ Dashboard/scripts/auth.py
@@ -6,7 +6,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 # from . import db
 # from flask_login import login_user, login_required, logout_user, current_user
 # </editor-fold>
-from scripts.compare import compare
+from Dashboard.scripts.compare import compare
 
 auth = Blueprint('auth', __name__)
 
