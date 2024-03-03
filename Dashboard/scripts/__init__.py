114abcac7e741dddaa071786e19889a45f8b7eaa
joyfuluj <joyumejiego@yahoo.com>
2024-03-02 18:32:05 -0800

    updated init

--- Dashboard/scripts/__init__.py
+++ Dashboard/scripts/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 DB_NAME = "database.db"
 
 
@@ -9,7 +9,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'r'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
+    # db.init_app(app)
 
     from .views import views
     from .auth import auth
