abc13629d0e6f50d7f429c690260f8f927c1178b
vol2468 <vol2468@student.ubc.ca>
2024-03-02 20:28:35 -0800

    confetti

--- Dashboard/scripts/static/js/compare.js
+++ Dashboard/scripts/static/js/compare.js
@@ -4,9 +4,12 @@ $(document).ready(function() {
     var winlose = $("#win-lose");
 
     if (myQuality > othQuality) {
-        winlose.html("You won!");
+        winlose.html("You win!");
+        var congrats = $('<script>confetti({particleCount: 100, spread: 70, origin: { y: 0.6 }});</script>');
+        $("body").append(congrats);
     } else if (myQuality < othQuality) {
-        winlose.html("You lost...");
+        winlose.html("You lose...");
+        $("#win-lose").css("color", "navy");
     } else {
         winlose.html("Almost! It's a draw.")
     }
