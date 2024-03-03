001e01e992f5f870cec53802971f05941cf6b1fa
leolelion <144266057+leolelion@users.noreply.github.com>
2024-03-02 16:29:43 -0800

    reorganised files

---
+++ Dashboard/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, Blueprint
+from scripts import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+"""
+
+
+
+@app.route('/')         
+def home():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+"""
+
+# @app.route('/login')
+# def login():
+#     return render_template('login.html')
+
---
+++ Dashboard/scripts/auth.py
@@ -0,0 +1,68 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+
+# <editor-fold desc="Description">
+# from .models import User
+# from . import db
+# from flask_login import login_user, login_required, logout_user, current_user
+# </editor-fold>
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@auth.route('/index')
+def index():
+    return render_template("index.html")
+
+
+@auth.route('/logout')
+def logout():
+    return render_template("logout.html")
+
+
+@auth.route('/register')
+def register():
+    return render_template("register.html")
+
+
+"""
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Logged in successfully!', category='success')
+            login_user(user, remember=True)
+            return redirect(url_for('views.home'))
+        else:
+            flash('Username does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        username = request.form.get('username')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Username already exists.', category='error')
+        elif len(username) < 4:
+            flash('Username must be greater than 3 characters.', category='error')
+        else:
+            new_user = User(username=username, method='sha256')
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html", user=current_user)
+"""""
